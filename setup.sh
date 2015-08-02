5a13704832c7b18761c36e8d3107e5818b2e4b0c
charles peters <charlespeters42@gmail.com>
2015-08-02 12:33:15 -0400

    show hidden files

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 # Xcode Nonsense
 xcodebuild -licence
 
+# Show Hidden Files
+defaults write com.apple.finder AppleShowAllFiles YES
+killall Finder
+
 # Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
