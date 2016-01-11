1f5674e50f92f5898a837ae0b3b9d04f13e63c02
charles peters <charlespeters42@gmail.com>
2016-01-10 18:09:06 -0800

    osx defaults

---
+++ osx.sh
@@ -0,0 +1,7 @@
+# Show Hidden Files
+defaults write com.apple.finder AppleShowAllFiles YES
+killall Finder
+
+
+# Sketch Settings
+defaults write com.bohemiancoding.sketch3 exportCompactSVG -bool yes
