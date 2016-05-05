36588d528c361d3c0d65dd558490f4e73ce5be6c
charles peters <charlespeters42@gmail.com>
2016-05-04 23:24:04 -0700

    combining config and setup

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,31 +1,4 @@
-# Vim
-
-mv ./vimrc ~/.vimrc
-
-vim +PluginInstall +qall
-vim +PlugInstall +qall
-
-# ZSH
-mv ./zshrc ~/.zshrc
-mv ./aliases ~/.aliases
-
-# Git Config
-mv ./gitconfig ~/.gitconfig
-mv ./gitignore ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
-
-# Hammerspoon
-mv ./hammerspoon.lua ~/.hammerspoon/init.lua
-
-# iTerm2 Color Palettes & Fonts
-cd ~/Downloads
-wget http://input.fontbureau.com/systemfont/InputSystemFontReplacement_ElCapitan.zip
-unzip InputSystemFontReplacement_ElCapitan.zip
-mv ~/Downloads/InputSystem_Fonts_ElCapitan/SystemFont_TTF/*.ttf /Library/Fonts/
-echo "Go to http://input.fontbureau.com/download/ to get the Input Mono for the Terminal"
-
-svn export https://github.com/mhartington/oceanic-next-iterm/trunk/
-echo "Import Oceanic Next in to Preferences > Profiles > Colors and then Load Presets > Import"
 
 # Setup NPM
 npm adduser
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,41 +1,90 @@
 # Try and Install Command Line Tools without Xcode
 xcode-select --install
 
-# Clean Up Homebrew
-rm -rf /usr/local/Cellar /usr/local/.git && brew cleanup
+setup-brew () {
+  # Clean Up Homebrew
+  rm -rf /usr/local/Cellar /usr/local/.git && brew cleanup
 
-# Install Homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  # Install Homebrew
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install Dependencies
-brew bundle
+  # Install Dependencies
+  brew bundle
 
-# Update brew
-brew update
+  # Update brew
+  brew update
+}
 
-# Update NPM
-npm update -g npm
+setup-zsh () {
+  curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+}
 
-# Install Oh My ZSH
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+setup-vim_tmux () {
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  ln -s ~/.vim ~/.config/nvim
+  ln -s ~/.vimrc ~/.config/nvim/init.vim
+  yes | vim +PluginClean! +PluginInstall! +qall
+
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+}
+
 
 # Install iTerm2 because I prefer it
-cd ~/Downloads
-wget http://iterm2.googlecode.com/files/iTerm2_v1_0_0.zip
-unzip iTerm2_v1_0_0.zip
-cp -r iTerm.app ~/Applications/ && echo "iTerm2 installed"
-cd ~/
+get-iterm () {
+  cd ~/Downloads
+  wget https://iterm2.com/downloads/beta/iTerm2-2_9_20160426.zip
+  unzip iTerm2-2_9_20160426.zip
+  cp -r iTerm.app ~/Applications/ && echo "iTerm2 installed"
+  cd ~/
+}
 
-# Install MacVim
-brew install macvim --with-override-system-vim
 
-# Install Vundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+setup-npm () {
+  npm install -g \
+    babel-cli \
+    browser-sync \
+    ember-cli \
+    empty-trash-cli \
+    eslint \
+    express-generator \
+    firebase-tools \
+    forever \
+    gulp-cli \
+    karma-cli \
+    nodemon \
+    postcss-cli \
+    stylelint \
+    surge \
+    trash-cli \
+    tldr \
+    vtop \
+    wifi-password
 
-# Install Common Node Globals
-npm install -g
+  npm update -g
+}
 
-# Python Dependencies
-sudo easy_install pip
-pip install codemod
-pip install thefuck
+setup-py () {
+  sudo easy_install pip
+  pip install \
+    thefuck \
+    codemod \
+}
+
+fix-iterm () {
+  # iTerm2 Color Palettes & Fonts
+  cd ~/Downloads
+  wget http://input.fontbureau.com/systemfont/InputSystemFontReplacement_ElCapitan.zip
+  unzip InputSystemFontReplacement_ElCapitan.zip
+  mv ~/Downloads/InputSystem_Fonts_ElCapitan/SystemFont_TTF/*.ttf /Library/Fonts/
+  echo "Go to http://input.fontbureau.com/download/ to get the Input Mono for the Terminal"
+
+  svn export https://github.com/mhartington/oceanic-next-iterm/trunk/
+  echo "Import Oceanic Next in to Preferences > Profiles > Colors and then Load Presets > Import"
+}
+
+setup-iterm () {
+  get-iterm
+  fix-iterm
+}
+
+setup-{brew,zsh,vim_tmux,npm,iterm}
