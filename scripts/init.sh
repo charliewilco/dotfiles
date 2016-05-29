40f68c56a4e8b9b6985297847a3c80c97593aa7f
charles peters <charlespeters42@gmail.com>
2016-05-29 01:17:36 -0700

    simple script to test

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,7 @@
-git clone https://github.com/charlespeters/dotfiles.git ~/.dotfiles
+# git clone https://github.com/charlespeters/dotfiles.git ~/.dotfiles
+#
+# cd ~/.dotfiles
+# sudo sh ./scripts/bootstrap.sh && sudo sh ./setup.sh
 
-cd ~/.dotfiles
-sudo sh ./scripts/bootstrap.sh && sudo sh setup.sh
+
+echo "I CAME FROM GITHUB"
