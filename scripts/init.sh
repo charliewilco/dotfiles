60d2f2769039142f4833ef2cdf49288d4bb41eeb
charles peters <charlespeters42@gmail.com>
2016-05-29 01:20:42 -0700

    more complex init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,4 @@
-# git clone https://github.com/charlespeters/dotfiles.git ~/.dotfiles
-#
-# cd ~/.dotfiles
-# sudo sh ./scripts/bootstrap.sh && sudo sh ./setup.sh
+git clone https://github.com/charlespeters/dotfiles.git ~/grownup-dotfiles
+cd ~/grownup-dotfiles
 
-
-echo "I CAME FROM GITHUB"
+sudo sh ./scripts/hello.sh
