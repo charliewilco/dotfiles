44f9c1d45fe3af1c8ffc9125da723c07831bbefb
c <c@zordon.local>
2017-04-03 18:45:01 -0700

    moving tmux config

---
+++ tmux/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if test $(which tmux)
+then
+  echo 'Setting up tmux'
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+  if "test ! -d ~/.tmux/plugins/tpm" \
+    "run 'git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && ~/.tmux/plugins/tpm/bin/install_plugins'"
+fi
+
+exit 0
