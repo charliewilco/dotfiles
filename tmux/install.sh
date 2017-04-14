04cc84f95e1d9be0d4d2ce56c2d1dee5a1cbbf5e
Charles Peters <cpeters@onvia.com>
2017-04-14 14:50:17 -0700

    updating tmux installer

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,10 +3,7 @@
 if test $(which tmux)
 then
   echo 'Setting up tmux'
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-  if "test ! -d ~/.tmux/plugins/tpm" \
-    "run 'git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && ~/.tmux/plugins/tpm/bin/install_plugins'"
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && ~/.tmux/plugins/tpm/bin/install_plugins
 fi
 
 exit 0
