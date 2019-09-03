079593235bdec68bcee51aa6473356269d559160
Charles ⚡ <charlespeters42@gmail.com>
2019-09-03 08:43:37 -0700

    Updating tmux

--- tmux/install.sh
+++ tmux/install.sh
@@ -7,6 +7,7 @@ then
 
   if "test ! -d ~/.tmux/plugins/tpm" \
     "run 'git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && ~/.tmux/plugins/tpm/bin/install_plugins'"
+
 fi
 
 exit 0
