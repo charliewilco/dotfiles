1e87275c23ea06043a91663c93726d9b5000aa57
Charlie ⚡ <charliewilc0@protonmail.com>
2022-09-28 09:40:44 -0700

    using bat and cleaning up tmux

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,14 +1,14 @@
 # Common
-alias p='pnpm'
 alias please='sudo'
-alias ls='ls -a'
+alias vim='nvim'
+alias ls='exa'
+alias cat='bat'
+alias tree='tree -h -I "node_modules*|bower_components*"'
+
 alias ..='cd ..'
 alias ...='cd ../../'
 alias ....='cd ../../../'
-alias dstry='find . -name ".DS_Store" -type f -delete'
 
-alias tree='tree -h -I "node_modules*|bower_components*"'
-alias cat='ccat'
+alias dstry='find . -name ".DS_Store" -type f -delete'
 alias empty='empty-trash'
 alias flx='hidden-files'
-alias vim='nvim'
