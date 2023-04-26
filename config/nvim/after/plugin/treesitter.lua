98a7b0ca636352c7762d3a310e1b09357430b5d0
Charlie ⚡ <charliewilc0@pm.me>
2023-04-26 10:27:43 -0700

    updating tmux

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup {
 	-- A list of parser names, or "all" (the four listed parsers should always be installed)
-	ensure_installed = { 'typescript', 'tsx', 'rust', 'lua', 'vim', 'help', 'css', 'html', 'graphql', 'prisma', 'yaml', 'json', 'toml' },
+	ensure_installed = { 'typescript', 'tsx', 'rust', 'vim', 'lua', 'css', 'html', 'graphql', 'prisma', 'yaml', 'json', 'toml' },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
