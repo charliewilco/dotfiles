e88ae07db1b6a32121206c928f1d365ee6d6b050
Charlie ⚡ <charliewilc0@pm.me>
2023-06-14 23:01:25 -0700

    adding lex keybinding

--- config/nvim/after/plugin/filetree.lua
+++ config/nvim/after/plugin/filetree.lua
@@ -1,3 +1,11 @@
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
 vim.g.netrw_winsize = 30
+
+
+local function set_rebindings()
+  local map_command = "<Esc>:Lex<CR>"
+  vim.api.nvim_set_keymap('n', '<Leader>t', map_command, { noremap = true, silent = true })
+end
+
+set_rebindings()
